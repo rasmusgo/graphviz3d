a3f2f6d907ed0f164809d694964aac7b69b13518
Rasmus Brönnegård <rasmus@volumental.com>
2023-04-27 09:28:54 +0200

    Add rerun and graphviz-rust stubs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
-fn main() {
-    println!("Hello, world!");
+use rerun::demo_util::grid;
+use rerun::external::glam;
+use rerun::{
+    components::{ColorRGBA, Point3D, Radius},
+    MsgSender,
+};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut session = rerun::SessionBuilder::new("my_app").buffered();
+
+    let points = grid(glam::Vec3::splat(-10.0), glam::Vec3::splat(10.0), 10)
+        .map(Point3D::from)
+        .collect::<Vec<_>>();
+    let colors = grid(glam::Vec3::ZERO, glam::Vec3::splat(255.0), 10)
+        .map(|v| ColorRGBA::from_rgb(v.x as u8, v.y as u8, v.z as u8))
+        .collect::<Vec<_>>();
+
+    MsgSender::new("my_points")
+        .with_component(&points)?
+        .with_component(&colors)?
+        .with_splat(Radius(0.5))?
+        .send(&mut session)?;
+
+    rerun::native_viewer::show(&session)?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+
+    use graphviz_rust::dot_generator::*;
+    use graphviz_rust::dot_structures::*;
+    use graphviz_rust::parse;
+
+    #[test]
+    fn parse_test() {
+        let g: Graph = parse(
+            r#"
+        strict digraph t {
+            aa[color=green]
+            subgraph v {
+                aa[shape=square]
+                subgraph vv{a2 -> b2}
+                aaa[color=red]
+                aaa -> bbb
+            }
+            aa -> be -> subgraph v { d -> aaa}
+            aa -> aaa -> v
+        }
+        "#,
+        )
+        .unwrap();
+
+        assert_eq!(
+            g,
+            graph!(strict di id!("t");
+            node!("aa";attr!("color","green")),
+            subgraph!("v";
+            node!("aa"; attr!("shape","square")),
+            subgraph!("vv"; edge!(node_id!("a2") => node_id!("b2"))),
+            node!("aaa";attr!("color","red")),
+            edge!(node_id!("aaa") => node_id!("bbb"))
+                ),
+                edge!(node_id!("aa") => node_id!("be") => subgraph!("v"; edge!(node_id!("d") => node_id!("aaa")))),
+                edge!(node_id!("aa") => node_id!("aaa") => node_id!("v"))
+            )
+        )
+    }
 }
