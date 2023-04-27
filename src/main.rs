a72e1103c2c866bf0cf88964a452bd5d683717e3
Rasmus Brönnegård <rasmus@volumental.com>
2023-04-27 12:04:02 +0200

    Plot nodes and edges in random positions

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,140 @@
-use rerun::demo_util::grid;
-use rerun::external::glam;
+use std::collections::HashMap;
+
+use graphviz_rust::dot_structures::*;
+use rand::{thread_rng, Rng};
 use rerun::{
-    components::{ColorRGBA, Point3D, Radius},
+    components::{Arrow3D, ColorRGBA, Label, Point3D, Radius},
     MsgSender,
 };
 
+pub fn id_to_string(id: &Id) -> String {
+    match id {
+        Id::Html(ref v) => format!("html {}", v),
+        Id::Escaped(ref v) => format!("esc {}", v),
+        Id::Plain(ref v) => format!("plain {}", v),
+        Id::Anonymous(ref v) => format!("anon {}", v),
+    }
+}
+
+pub fn port_to_string(port: &Port) -> String {
+    match port {
+        Port(None, None) => "".to_string(),
+        Port(Some(ref id), None) => id_to_string(id),
+        Port(None, Some(ref dir)) => format!(":{}", dir),
+        Port(Some(ref id), Some(ref dir)) => format!("{}:{}", id_to_string(id), dir),
+    }
+}
+
+pub fn node_id_to_string(node_id: &NodeId) -> String {
+    match node_id.1 {
+        None => id_to_string(&node_id.0),
+        Some(ref port) => format!("{}:{}", id_to_string(&node_id.0), port_to_string(port)),
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let session = rerun::SessionBuilder::new("my_app").buffered();
+    let mut rng = thread_rng();
 
-    let points = grid(glam::Vec3::splat(-10.0), glam::Vec3::splat(10.0), 10)
-        .map(Point3D::from)
-        .collect::<Vec<_>>();
-    let colors = grid(glam::Vec3::ZERO, glam::Vec3::splat(255.0), 10)
-        .map(|v| ColorRGBA::from_rgb(v.x as u8, v.y as u8, v.z as u8))
-        .collect::<Vec<_>>();
+    let dot =
+        std::fs::read_to_string("/home/rasmusgo/src/Reconstruction/docs/graphviz/cmake/gg.dot")?;
+    let g: Graph = graphviz_rust::parse(dot.as_str())?;
+    let statements = match g {
+        Graph::Graph {
+            id: _,
+            strict: _,
+            ref stmts,
+        }
+        | Graph::DiGraph {
+            id: _,
+            strict: _,
+            ref stmts,
+        } => stmts,
+    };
 
-    MsgSender::new("my_points")
+    let session = rerun::SessionBuilder::new("my_app").connect(rerun::default_server_addr());
+
+    let nodes: HashMap<String, Node> = statements
+        .iter()
+        .filter_map(|s| match s {
+            Stmt::Node(n) => Some((node_id_to_string(&n.id), n.clone())),
+            _ => None,
+        })
+        .collect();
+
+    let mut node_indices = HashMap::<String, usize>::new();
+    for key in nodes.keys() {
+        node_indices.insert(key.clone(), node_indices.len());
+    }
+    let node_indices = node_indices;
+    let num_points = node_indices.len();
+
+    let mut edges = Vec::<(NodeId, NodeId)>::new();
+    for s in statements {
+        match s {
+            Stmt::Edge(Edge {
+                ty: EdgeTy::Pair(Vertex::N(a), Vertex::N(b)),
+                attributes: _,
+            }) => edges.push((a.clone(), b.clone())),
+            Stmt::Edge(Edge {
+                ty: EdgeTy::Chain(chain),
+                attributes: _,
+            }) => {
+                for i in 0..chain.len() - 1 {
+                    if let (Vertex::N(a), Vertex::N(b)) = (&chain[i], &chain[i + 1]) {
+                        edges.push((a.clone(), b.clone()))
+                    }
+                }
+            }
+            _ => (),
+        }
+    }
+    let edges = edges;
+
+    let mut points = Vec::with_capacity(node_indices.len());
+    for _ in 0..num_points {
+        let point = Point3D {
+            x: rng.gen_range(-1.0..1.0),
+            y: rng.gen_range(-1.0..1.0),
+            z: rng.gen_range(-1.0..1.0),
+        };
+        points.push(point);
+    }
+
+    let mut colors = Vec::with_capacity(node_indices.len());
+    for _ in 0..num_points {
+        colors.push(ColorRGBA::from_rgb(
+            rng.gen_range(0..255),
+            rng.gen_range(0..255),
+            rng.gen_range(0..255),
+        ));
+    }
+    let colors = colors;
+    let labels = nodes.keys().map(|k| Label(k.clone())).collect::<Vec<_>>();
+    MsgSender::new("nodes")
         .with_component(&points)?
         .with_component(&colors)?
-        .with_splat(Radius(0.5))?
+        .with_component(&labels)?
+        .with_splat(Radius(0.05))?
         .send(&session)?;
 
-    rerun::native_viewer::show(&session)?;
+    let arrows = edges
+        .iter()
+        .map(|(a, b)| {
+            let a = node_indices.get(&node_id_to_string(a)).unwrap();
+            let b = node_indices.get(&node_id_to_string(b)).unwrap();
+            let a = &points[*a];
+            let b = &points[*b];
+            Arrow3D {
+                origin: [a.x, a.y, a.z].into(),
+                vector: [b.x - a.x, b.y - a.y, b.z - a.z].into(),
+            }
+        })
+        .collect::<Vec<_>>();
+    MsgSender::new("edges")
+        .with_component(&arrows)?
+        .send(&session)?;
+
+    // rerun::native_viewer::show(&session)?;
 
     Ok(())
 }
